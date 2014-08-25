2488d18817485d056a8a3e28558d655ba664d9ed
Rahul Gandhi <gandhi.rahul93@gmail.com>
2014-08-25 16:28:56 +0530

    added other files

---
+++ background.js
@@ -0,0 +1,9 @@
+chrome.browserAction.onClicked.addListener(function(tab){
+	//alert("Tweeting " + tab.url + " " + tab.title);
+	var base_url = "https://twitter.com/intent/tweet?";
+	var url = encodeURIComponent(tab.url);
+	var text = tab.title;
+	var tweet_url = base_url + "text=" + text + "&url=" + url;
+	//alert(tweet_url);
+	chrome.tabs.create({"url":tweet_url, "selected":true});
+});
