7b4c2c0bc9fc7f1ba661ce399ebce187c93f2335
Rahul Gandhi <gandhi.rahul93@gmail.com>
2014-08-27 13:05:26 +0530

    encoded text field in tweet intent

--- background.js
+++ background.js
@@ -2,7 +2,7 @@ chrome.browserAction.onClicked.addListener(function(tab){
 	//alert("Tweeting " + tab.url + " " + tab.title);
 	var base_url = "https://twitter.com/intent/tweet?";
 	var url = encodeURIComponent(tab.url);
-	var text = tab.title;
+	var text = encodeURIComponent(tab.title);
 	var tweet_url = base_url + "text=" + text + "&url=" + url;
 	//alert(tweet_url);
 	chrome.tabs.create({"url":tweet_url, "selected":true});
